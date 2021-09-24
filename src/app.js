d41860e36f5a46462465d82b283854864c55de8d
unknown <ncnilesh145@gmail.com>
2021-09-24 19:26:01 +0530

    updated command to get port no. from env

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const forecast=require('./utils/forecast')
 //console.log(path.join(__dirname,'../public'))
 
 const app=express()
-const port=process.env.port || 3000
+const port=process.env.PORT || 3000
 
 //Define paths for Express config
 const publicDirectoryPath=path.join(__dirname,'../public')
